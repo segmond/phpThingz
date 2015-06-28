3c907711cc79ac7f6d8e545e3ae2c89e7c16f272
Uzo AtHome <segmond@gmail.com>
2015-06-28 11:31:48 -0400

    singleton demo

--- prototype/Client.php
+++ prototype/Client.php
@@ -14,6 +14,26 @@ class Client
     {
         $this->makeConcretePrototype();
 
+        $Tess = clone $this->marketer;
+        $this->setEmployee($Tess, 'Tess Smith', 101, 'ts101-1234', 'tess.png');
+        $this->showEmployee($Tess);
+
+        $Jacob = clone $this->marketer;
+        $this->setEmployee($Jacob, 'Jacob Jones', 102, 'jj102-2234', 'jacob.png');
+        $this->showEmployee($Jacob);
+
+        $Ricky = clone $this->manager;
+        $this->setEmployee($Ricky, 'Ricky Rod', 203, 'rr102-2234', 'jacob.png');
+        $this->showEmployee($Ricky);
+
+        $Olivia = clone $this->engineer;
+        $this->setEmployee($Olivia, 'Olivia Perez', 302, 'op102-2234', 'olivia.png');
+        $this->showEmployee($Olivia);
+
+        $John = clone $this->engineer;
+        $this->setEmployee($John, 'John Jackson', 301, 'jj302-2234', 'john.png');
+        $this->showEmployee($John);
+
     }
 
     private function makeConcretePrototype()
@@ -25,10 +45,19 @@ class Client
 
     private function showEmployee(IACmePrototype $employeeNow)
     {
+        $px = $employeeNow->getPic();
+        echo "$px\n";
+        echo $employeeNow->getName() . "\n";
+        echo $employeeNow->getDept() . "\n";
+        echo $employeeNow->getID() . "\n";
     }
 
     private function setEmployee(IACmePrototype $employeeNow, $nm, $dp, $id, $px)
     {
+        $employeeNow->setName($nm);
+        $employeeNow->setDept($dp);
+        $employeeNow->setID($id);
+        $employeeNow->setPic("pix/$px");
     }
 }
 
---
+++ singleton/SingletonDemo.php
@@ -0,0 +1,48 @@
+<?php
+
+// Lazy initialization
+class SingletonDemo {
+    private static $instance = null;
+    private $registry = array();
+
+    private function __construct() { }
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new SingletonDemo();
+        }
+        return self::$instance;
+    }
+
+    public function setRegister($key, $val) {
+        $this->registry[$key] = $val;
+    }
+
+    public function getRegister($key) {
+        return $this->registry[$key];
+    }
+}
+
+
+$s = SingletonDemo::getInstance();
+
+$s->setRegister('MI', 'Michigan');
+$s->setRegister('NY', 'New York');
+$s->setRegister('OH', 'Ohio');
+
+echo $s->getRegister('NY') . "\n";
+echo $s->getRegister('OH') . "\n";
+echo $s->getRegister('MI') . "\n";
+
+unset($s);
+
+$t = SingletonDemo::getInstance();
+echo $t->getRegister('NY') . "\n";
+echo $t->getRegister('OH') . "\n";
+echo $t->getRegister('MI') . "\n";
+
+$y = $t;
+echo $y->getRegister('NY') . "\n";
+
+// below will fail
+$z = new SingletonDemo();
