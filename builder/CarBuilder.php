ad6de1049de8d7c0954e1c0bd00f42d40700549f
Uzo AtHome <segmond@gmail.com>
2015-07-11 23:52:36 -0400

    builder

---
+++ builder/CarBuilder.php
@@ -0,0 +1,6 @@
+<?php
+
+/** Car has too many options, combation of each option would lead to a huge list of constructors.
+    So we will create a builder class CarBuilder, we will send it each car option set by step
+    and then construct the final car with the right options.
+*/
