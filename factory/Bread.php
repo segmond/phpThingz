a0cc7924fe81148bac5a1c85cf0a2fbdb27dd4db
Segmond <segmond@gmail.com>
2016-09-20 21:32:14 -0400

    expanded for more demonstration

--- factory/Bread.php
+++ factory/Bread.php
@@ -10,16 +10,37 @@ class BananaBread implements Bread {
     }
 }
 
-interface BreadFactory {
-    public function makeBread();
-}
-
-class BananaBreadFactory implements BreadFactory {
-    public function makeBread() {
-        return new BananaBread();
+class PitaBread implements Bread {
+    public function getType() {
+        return 'Pita Bread';
     }
 }
 
-$factory = new BananaBreadFactory();
-$bread = $factory->makeBread();
+interface IBreadFactory {
+    public function makeBread($type);
+}
+
+class BreadFactory implements IBreadFactory {
+
+	public function makeBread($bread_type) {
+		switch (strtoupper($bread_type)) {
+			case 'BANANA':
+				return $this->makeBananaBread();
+			case 'PITA':
+				return $this->makePitaBread();
+			default:
+				return null;
+		}
+
+	}
+    public function makeBananaBread() {
+        return new BananaBread();
+    }
+    public function makePitaBread() {
+        return new PitaBread();
+    }
+}
+
+$factory = new BreadFactory();
+$bread = $factory->makeBread('pita');
 echo $bread->getType();
