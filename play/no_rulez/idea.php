4cbf0beb6dba8db774dd67ec8903339f3371be81
Segmond <segmond@gmail.com>
2017-01-22 22:56:27 -0500

    Added quick test

--- play/no_rulez/idea.php
+++ play/no_rulez/idea.php
@@ -2,13 +2,18 @@
 // Avoid implementing your own rule engine, first pass
 //
 // https://hackernoon.com/you-might-not-need-if-statements-a-better-approach-to-branching-logic-59b4f877697f#.rp1v5j8wc
-class User { }
+class User { 
+	public $signed_in = false;
+	public $is_premium = false;
+	public $searched = false;
+	public $display = null;
+	public $search_category = null;
+}
+
 class SignedInUser extends User {
-	private $signed_in = false;
-	private $is_premium = false;
-	private $display = null;
 	public function __construct(Display $display) {
 		$this->display = $display;
+		$this->signed_in = true;
 	}
 	public function render() {
 		if ($this->signed_in) {
@@ -22,8 +27,6 @@ class SignedInUser extends User {
 }
 
 class Visitor extends User {
-	private $searched = false;
-	private $display = null;
 	public function __construct(Display $display) {
 		$this->display = $display;
 	}
@@ -31,16 +34,18 @@ class Visitor extends User {
 		if (!$this->searched) {
 			$this->display->showMainBannerAd();
 		} else {
-			$this->display->showAdsByCategory($search_category);
+			$this->display->showAdsByCategory($this->search_category);
 		}
 	}
 }
 
 class Display {
-	public function showSupportWidget() { }
-	public function showTipOfTheDay() { }
-	public function showMainBannerAd() { }
+	public $nappies_ad_available=true;
+	public function showSupportWidget() { echo "Support Widget\n"; }
+	public function showTipOfTheDay() { echo "Tip of the Day\n";}
+	public function showMainBannerAd() { echo "Main Banner Ad\n"; }
 	public function showAdsByCategory($search_category) { 
+		//var_dump($search_category);
 		switch ($search_category) {
 		case 'SINGLES':
 			$this->showKittenAd();
@@ -53,13 +58,55 @@ class Display {
 			break;
 		}
 	}
-	private function showKittenAd() { }
+	private function showKittenAd() { echo "Kitten Ad\n"; }
 	private function showNappiesAd() { 
-		if ($nappies_ad_available) {
-			// Nappies Ad
+		if ($this->nappies_ad_available) {
+			echo "Nappies Ad\n";
 		} else {
 			$this->showMainBannerAd();
 		}
 	}
-	private function showHolidayAd() { }
+	private function showHolidayAd() { echo "Holiday Ad\n"; }
 }
+
+class UserTest {
+	public function __construct(User $u) {
+		$this->user = $u;
+	}
+	public function test() {
+		$true_false = [true, false];
+		$signed_in = $true_false;
+		$is_premium = $true_false;
+		$searched = $true_false;
+		$search_category = ['SINGLES', 'WEDDING', 'RETIREMENT', 'FOO'];
+		foreach ($signed_in as $s) {
+			foreach ($is_premium as $p) {
+				foreach ($searched as $se) {
+					//var_dump($search_category);
+					foreach ($search_category as $sc) {
+						$this->user->signed_in = $s;
+						$this->user->is_preimum = $p;
+						$this->user->searched = $se;
+						$this->user->search_category = $sc;
+						//var_dump(['signed_in'=>$this->signed_in, 'premium'=>$this->is_preimum, 
+						//	'searched'=>$this->searched, 'category'=>$this->search_category]);
+						$this->user->render();
+					}
+				}
+			}
+		}
+	}
+}
+
+// Test all possible combinations
+$d = new Display();
+$u = new UserTest(new SignedInUser($d));
+$u->test();
+
+$v = new UserTest(new Visitor($d));
+$v->test();
+
+$d = new Display();
+$d->nappies_ad_available=false;
+$v = new UserTest(new Visitor($d));
+$v->test();
